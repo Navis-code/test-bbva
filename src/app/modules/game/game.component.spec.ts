9812631f0dbab1ec2262157f9ade9eb91691eb39
Navis-code <info@naviscode.com>
2022-06-17 23:28:45 +0200

    test: game component

--- src/app/modules/game/game.component.spec.ts
+++ src/app/modules/game/game.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { GameConfigI, GameI } from '../../models/Game';
+import { UserI } from '../../models/User';
 import { AuthService } from '../../services/auth/auth.service';
+import { GAMES } from '../game/config/game.config';
 import { ModalGameSelectionComponent } from './components/modal-game-selection/modal-game-selection.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
@@ -11,6 +14,7 @@ describe('GameComponent', () => {
   let fixture: ComponentFixture<GameComponent>;
 
   beforeEach(async () => {
+    Object.defineProperty(window.navigator, 'vibrate', {});
     await TestBed.configureTestingModule({
       providers: [AuthService],
       imports: [RouterTestingModule],
@@ -28,5 +32,33 @@ describe('GameComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should play and save currentUser', async () => {
+    const selection: GameConfigI = {
+      name: 'Rock',
+      beats: ['Scissors'],
+      visual: '✊',
+    };
+    const user: UserI = {
+      name: 'testUser',
+      gameStats: {
+        draw: 0,
+        lose: 0,
+        win: 0,
+      },
+    };
+    const gameServiceSpy = jest.spyOn(component.gameService, 'play');
+    const storageSpy = jest.spyOn(component.authService, 'saveCurrentUser');
+    await component.playerSelection(selection, user);
+
+    expect(gameServiceSpy).toHaveBeenCalled();
+    expect(storageSpy).toHaveBeenCalled();
+  });
+
+  it('should change game', () => {
+    const game: GameI = GAMES[1];
+    component.changeToOtherGame(game);
+    expect(component.gameService.game).toEqual(game);
   });
 });
