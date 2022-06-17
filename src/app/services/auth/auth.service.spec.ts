b42a02647d3f06c31a2c28afb97550180293e3ac
Navis-code <info@naviscode.com>
2022-06-17 23:02:05 +0200

    test: add test to auth service

--- src/app/services/auth/auth.service.spec.ts
+++ src/app/services/auth/auth.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { GameComponent } from '../../modules/game/game.component';
 
 import { AuthService } from './auth.service';
 
@@ -8,12 +9,40 @@ describe('AuthService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: 'game', component: GameComponent },
+        ]),
+      ],
     });
     service = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('shoud init user and navigate to game', () => {
+    const userName = 'testUser';
+    const routerSpy = jest.spyOn(service['router'], 'navigate');
+    service.initUser(userName);
+    expect(service.isAuthenticated()).toBeTruthy();
+    expect(routerSpy).toHaveBeenCalledWith(['/game']);
+  });
+
+  it('shoud create new user and save it to storage', () => {
+    const userName = 'testUser';
+    const storageSpy = jest.spyOn(service['storage'], 'setNewUser');
+    service['createNewUser'](userName);
+    expect(storageSpy).toHaveBeenCalledWith({
+      name: userName,
+      gameStats: { win: 0, lose: 0, draw: 0 },
+    });
+  });
+
+  it('return true if user is authenticated', () => {
+    const userName = 'testUser';
+    service.initUser(userName);
+    expect(service.isAuthenticated()).toBeTruthy();
   });
 });
