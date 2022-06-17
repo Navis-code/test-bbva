b73fc40ffebbe4460f85d3a81fe8a339b1799d0e
Navis-code <info@naviscode.com>
2022-06-17 21:24:40 +0200

    test: add result test

--- src/app/modules/game/services/game.service.spec.ts
+++ src/app/modules/game/services/game.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { ResultsEnum } from '../../../models/Game';
 import { GAMES } from '../config/game.config';
 
 import { GameService } from './game.service';
@@ -25,5 +26,24 @@ describe('GameService', () => {
     const game = GAMES[1];
     service.changeGame(game);
     expect(service.game).toBe(game);
+  });
+
+  it('should return WIN when ✊ vs ✌', () => {
+    const selection1 = { beats: ['Scissors'], name: 'Rock', visual: '✊' };
+    const selection2 = { beats: ['Paper'], name: 'Scissors', visual: '✌' };
+
+    expect(service['result'](selection1, selection2)).toBe(ResultsEnum.WIN);
+  });
+  it('should return LOSE when ✌ vs ✊', () => {
+    const selection1 = { beats: ['Paper'], name: 'Scissors', visual: '✌' };
+    const selection2 = { beats: ['Scissors'], name: 'Rock', visual: '✊' };
+
+    expect(service['result'](selection1, selection2)).toBe(ResultsEnum.LOSE);
+  });
+  it('should return DRAW when ✌ vs ✌', () => {
+    const selection1 = { beats: ['Paper'], name: 'Scissors', visual: '✌' };
+    const selection2 = { beats: ['Paper'], name: 'Scissors', visual: '✌' };
+
+    expect(service['result'](selection1, selection2)).toBe(ResultsEnum.DRAW);
   });
 });
