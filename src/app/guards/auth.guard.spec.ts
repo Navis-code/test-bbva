fbebc73a8cb2dcd2b535a73326f2e3081a90bcee
Navis-code <info@naviscode.com>
2022-06-17 23:10:53 +0200

    test: auth guard and remove unused params of can activate

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { GameComponent } from '../modules/game/game.component';
 
 import { AuthGuard } from './auth.guard';
 
@@ -8,12 +9,31 @@ describe('AuthGuard', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: 'game', component: GameComponent },
+        ]),
+      ],
     });
     guard = TestBed.inject(AuthGuard);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('should navigate to game if is authenticated', () => {
+    const routerSpy = jest.spyOn(guard['router'], 'navigate');
+    const authSpy = jest.spyOn(guard['authService'], 'isAuthenticated');
+    const authUser = guard['authService'].initUser('testUser');
+    guard.canActivate();
+    expect(routerSpy).toHaveBeenCalledWith(['/game']);
+  });
+  it('should navigate to home if is not authenticated', () => {
+    const routerSpy = jest.spyOn(guard['router'], 'navigate');
+    const authSpy = jest.spyOn(guard['authService'], 'isAuthenticated');
+    authSpy.mockReturnValue(false);
+    guard.canActivate();
+    expect(routerSpy).toHaveBeenCalledWith(['']);
   });
 });
